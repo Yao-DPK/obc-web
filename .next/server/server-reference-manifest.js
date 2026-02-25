80275f93dccdba01eda749b0d5617b1ac00b96d4
Konan Yao <yao.konan2709@gmail.com>
2026-02-25 19:55:53 +0100

    Revert "Premiere Page"
    This reverts commit 7b0bdc0a028bc7d77ad66cb56bf5ce2020692aa1.

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aB+isJjmE/ZOMEgcXTdcOL5YyLH5/H4c1DTLjEQKYNU=\"\n}"
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Image from 'next/image'
- 
-export default function Page() {
-  return <Image src="/OBC.jpeg" alt="Profile" width={100} height={100} />
-}
